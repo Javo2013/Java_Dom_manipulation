3a7ccf4667f6a021924d0d210ed42cc29199b688
Javian Williams <javian.williams29@gmail.com>
2025-12-29 13:45:16 -0500

    theme set up & profile card update

--- profile_card.js
+++ profile_card.js
@@ -9,3 +9,31 @@ const profileCard = document.getElementById("profile-card");
 const profileName = document.getElementById("profile-name");
 const profileTitle = document.getElementById("profile-title");
 const profileBio = document.getElementById("profile-bio");
+
+
+let updateCount = 0;
+
+const counterEl = document.createElement("p");
+counterEl.id = "update-counter";
+counterEl.classList.add("hidden"); //should only show after the first update
+counterEl.textContent = "updated 0 times";
+
+//Counter for under my bio
+profileBio.insertAdjacentElement("afterend", counterEl);
+
+
+// ==== Class Theme setup ====
+const styleTag = document. creationElement("style");
+styleTag.textcontent = `
+
+.profile-card.dark {
+    background: #111;
+    color: #f5f5f5;
+    border-color: #f5f5f5;
+  }
+  body.dark {
+    background: #0b0b0b;
+    color: #f5f5f5;
+  }
+`;
+document.head.appendChild(styleTag);
