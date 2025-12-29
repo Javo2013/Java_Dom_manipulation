dcf7d580202448b980b2c73c1ac15d4370953c4c
Javian Williams <javian.williams29@gmail.com>
2025-12-29 13:23:30 -0500

    Element creation

---
+++ profile_card.js
@@ -0,0 +1,11 @@
+const nameInput = document.getElementById("name-input");
+const titleInput = document.getElementById("title-input");
+const bioInput = document.getElementById("bio-input");
+
+const updateBtn = document.getElementById("update-card");
+const themeBtn = document.getElementById("toggle-theme");
+
+const profileCard = document.getElementById("profile-card");
+const profileName = document.getElementById("profile-name");
+const profileTitle = document.getElementById("profile-title");
+const profileBio = document.getElementById("profile-bio");
